ad4c486a1229a8ec30d2c291c3e14c7ed67cc1aa
0701MF20 <farazmohd0701@gmail.com>
2021-07-23 01:17:27 +0530

    6th branch updated with json component applicable(WORKING)+having string separtated separataed+magnitude as decimal+date+update of UI in better form

--- app/src/main/java/com/example/quakereport/QuakesinfoAdapter.java
+++ app/src/main/java/com/example/quakereport/QuakesinfoAdapter.java
@@ -30,13 +30,31 @@ public class QuakesinfoAdapter extends ArrayAdapter<Quakesinfo> {
         DecimalFormat formatter=new DecimalFormat("0.0");
         String output=formatter.format(adapter.getMag());
         t1.setText(output);
+        /*TextView t2=(TextView)listview.findViewById(R.id.name_id);
+        t2.setText(adapter.getName());*/
         TextView t2=(TextView)listview.findViewById(R.id.name_id);
-        t2.setText(adapter.getName());
-        TextView t3=(TextView)listview.findViewById(R.id.date_id);
+        TextView t22=(TextView)listview.findViewById(R.id.name2_id);
+        String place=adapter.getName();
+        String[] place_array=place.split("of");
+        String place1=place_array[0];
+        String place2=place_array[1];
+        t2.setText(place1);
+        t22.setText(place2);
+        /*TextView t3=(TextView)listview.findViewById(R.id.date_id);
         Date dates=new Date(adapter.getDate());
         SimpleDateFormat formatted=new SimpleDateFormat("dd-mm-yyyy, HH:MM:SS a");
         String outputs=formatted.format(dates);
-        t3.setText(outputs);
+        t3.setText(outputs);*/
+        TextView t3=(TextView)listview.findViewById(R.id.date_id);
+        TextView t33=(TextView)listview.findViewById(R.id.date2_id);
+        Date dates=new Date(adapter.getDate());
+        SimpleDateFormat formatted=new SimpleDateFormat("dd-mm-yyyy HH:MM:SS a");
+        String final_date=formatted.format(dates);
+        String[] array_date=final_date.split(" ",1);
+        String date_1=array_date[0];
+        String date_2=array_date[1];
+        t3.setText(date_1);
+        t33.setText(date_2);
         return listview;
     }
 }
